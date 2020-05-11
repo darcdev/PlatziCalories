28ca41fd1933b8f775fc201ada6b6c553ebd707e
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-11 16:20:52 -0500

    validate inputs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,32 @@ const compose = (...functions) => data =>
     functions.reduceRight((value, func) => func(value), data)
 
 
-const $calories = $("#calories").val();
-const $carbs = $("#carbs").val();
-const $protein = $("#protein").val();
-const $description = $("#description").val();
+const $description = $("#description");
+const $calories = $("#calories");
+const $carbs = $("#carbs");
+const $protein = $("#protein");
+
+$description.keypress(() => {
+    $description.removeClass('is-invalid');
+})
+$calories.keypress(() => {
+    $calories.removeClass('is-invalid');
+})
+$carbs.keypress(() => {
+    $carbs.removeClass('is-invalid');
+})
+$protein.keypress(() => {
+    $protein.removeClass('is-invalid');
+})
+
+const validateInputs = () => {
+
+    $description.val() ? '' : $description.addClass('is-invalid')
+    $calories.val() ? '' : $calories.addClass('is-invalid')
+    $carbs.val() ? '' : $carbs.addClass('is-invalid')
+    $protein.val() ? '' : $protein.addClass('is-invalid')
+
+    if ($description.val() && $calories.val() && $carbs.val() && $protein.val()) {
+        console.log('agregar informacion');
+    }
+}
