3497363149510b25408eadd3ca6a2bf17215b9e8
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-11 15:39:46 -0500

    get input values

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 const compose = (...functions) => data =>
-  functions.reduceRight((value, func) => func(value), data)
+    functions.reduceRight((value, func) => func(value), data)
+
+
+const $calories = $("#calories").val();
+const $carbs = $("#carbs").val();
+const $protein = $("#protein").val();
+const $description = $("#description").val();
